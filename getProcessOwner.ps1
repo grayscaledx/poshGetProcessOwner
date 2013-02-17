2b287caee1dac5b9be32edba8ce9eac9f9b3405f
gray <agentdeployed@gmail.com>
2013-02-17 16:55:53 -0500

    Localhost processing successful
    win32_process returning default output as expected

--- getProcessOwner.ps1
+++ getProcessOwner.ps1
@@ -6,20 +6,27 @@ Queries target computer for current processes and collects process owners, proce
 Queries the target computer for all processes that are running on the selected target or targets.  The level of privilege used to execute
 the function will dictate the scope of the information returned.  If you're looking for all processes to be returned from a target destination,
 the appropriate administrative level will be required.
-.PARAMETER
-
+.PARAMETER ComputerName
+The name or IP address of the target host to query.
 .EXAMPLE
-
+Hello World!
 #>
 [CmdletBinding()]
 param(
-
+    [Parameter(HelpMessage="A target computer host name or IP address to query.",
+               ValueFromPipeline=$True,
+               ValueFromPipelineByPropertyName=$True)]
+    [string[]]$ComputerName = 'localhost'
 )
 
 
-BEGIN {}
+BEGIN {
+    $ComputerProcesses = $ComputerName | ForEach-Object { Get-WmiObject -ComputerName $ComputerName win32_process }
+}
 PROCESS {
-
+    foreach ($TargetComputer in $ComputerProcesses){
+        Write-Output $TargetComputer
+    }
 }
 END {}
 
