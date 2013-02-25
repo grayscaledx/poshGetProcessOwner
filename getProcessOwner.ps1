92ed17251a7942ceae12e594a835c9818dcb2f96
gray <agentdeployed@gmail.com>
2013-02-24 20:33:01 -0500

    Cleaned up function summary and debug comments

--- getProcessOwner.ps1
+++ getProcessOwner.ps1
@@ -2,17 +2,40 @@
 <#
 .SYNOPSIS
 Queries target computer for current processes and collects process owners, process name, and command line arguments used for execution.
+
 .DESCRIPTION
 Queries the target computer for all processes that are running on the selected target or targets.  The level of privilege used to execute
 the function will dictate the scope of the information returned.  If you're looking for all processes to be returned from a target destination,
 the appropriate administrative level will be required.
 
 The current version of the function will only passthrough the current account the function is executed as to any remote systems.
+
 .PARAMETER ComputerName
 The name or IP address of the target host to query.
+
 .EXAMPLE
-Get-ProcessOwner -ComputerName dathost
-Gets all processes on target 'dathost'
+Get-ProcessOwner -ComputerName 192.168.100.15
+Returns all processes on target host at IP address 192.168.100.15.
+
+.EXAMPLE
+Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object {$_.IsService -eq $True}
+(This example is compatible with PowerShell 2.0 and later)
+Returns all services running on target host ApplicationServer01.contoso.com.
+
+.EXAMPLE
+Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object -Property IsService -eq $True
+(This example is compatible with PowerShell 3.0 and later)
+Returns all services running on target host ApplicationServer01.contoso.com.
+
+.EXAMPLE
+Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object {$_.ProcessOwner -match "appSvcUser"}
+(This example is compatible with PowerShell 2.0 and later)
+Returns all processes running under the appSvcUser user context (irrespective of domain) on target host ApplicationServer01.contoso.com.
+
+.EXAMPLE
+Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object -Property ProcessOwner -eq "SYSTEM"
+(This example is compatible with PowerShell 3.0 and later)
+Returns all processes running under the local SYSTEM user context on target host ApplicationServer01.contoso.com.
 #>
 [CmdletBinding()]
 param(
@@ -21,9 +44,7 @@ param(
                ValueFromPipelineByPropertyName=$True)]
     [string[]]$ComputerName = 'localhost',
     [Parameter(HelpMessage="A valid username to check processes against.")]
-    $UserAccount = $null,
-    [Parameter(HelpMessage="Enable if you want to filter for service ownership only")]
-    [switch]$ServicesOnly
+    $UserAccount = $null
 )
 
 
@@ -43,12 +64,7 @@ PROCESS {
 
             Write-Verbose "Processes Received; Building Query Object..."
             foreach ($ComputerProcess in $CurrentProcesses){           
-                
-                #Write-Debug "ComputerProcess PID is $($ComputerProcess.ProcessID) and type is $($ComputerProcess.ProcessID.GetType().Fullname)"
-                #Write-Debug "ComputerServices PIDs are $($CurrentServices.ProcessID) and type is $($CurrentServices.ProcessID.GetType().Fullname)"
-                #Write-Debug "Is the current process ID in the service pool? $($CurrentServices.ProcessID -contains $ComputerProcess.ProcessID)`n`n"
 
-                #Write-Verbose "Checking if ProcessID $ComputerProcess.ProcessID is a service..."
                 if ($CurrentServices.ProcessID -contains $ComputerProcess.ProcessID){
                     Write-Verbose "ProcessID $($ComputerProcess.ProcessID) `/ $($ComputerProcess.ProcessName) is a service..."
                     $isService = $True
@@ -66,18 +82,7 @@ PROCESS {
                                  }
                 $processObj = New-Object -TypeName psobject -Property $processProps
                 $processContainer += $processObj
-                
-                <#
-                Found the way to compare running process IDs and service IDs to filter for services
-                running under a user context using Compare-Object
-                
-                $processes = Get-Wmiobject -ComputerName <computerName> win32_process
-                $services = Get-Wmiobject -ComputerName <computerName> win32_service
-                $servicePids = Compare-Object $processes.ProcessID $services.ProcessID -IncludeEqual -ExcludeDifferent | Sort-Object -Property InputObject | Select-Object -ExpandProperty InputObject
-                
-                $servicePids will contain a sorted list of process IDs of services.  This is only required to tie to user $processes.GetOwner().User for services user ownership.
-                #>
-
+                            
             }
         }
 }
