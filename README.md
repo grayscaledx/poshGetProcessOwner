08bdd776732f8ecc41e9a72556651ff97650f3ee
gray <agentdeployed@gmail.com>
2013-03-03 22:33:57 -0500

    Completed progress information and error handling
    Completed two of the three To Do items on the list.  Also added thanks
    to README to those people in the community who've been a great help and
    influence.

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ poshGetProcessOwner
 PowerShell function used to help find user-context ownership for processes/services
 
 TODO List
-+ Add progress information
-+ Add proper error logging
-+ Finish adding functionality for alternate credentials
++ Add progress information - COMPLETED
++ Add proper error logging - COMPLETED (debating taking directly from $Error so the error object type isn't lost as a result of -ErrorAction)
++ Finish adding functionality for alternate credentials (Get-WmiObject throws an error saying it will not accept credentials for local queries)
+
+Special to the following people/resources:
+
+@concentrateddon - Don Jones
+@darkoperator - Carlos Perez
+@juneb_get_help - June Blender and MS Help Documentation Team et. al (Get-Help -eq "Awesome")
+@JeffHicks - Jeffery Hicks
+Manning Publications (Good technical books that aren't a dry read!)
--- getProcessOwner.psm1
+++ getProcessOwner.psm1
@@ -1,4 +1,6 @@
-﻿Function Get-ProcessOwner{
+﻿$logLocation = "$env:HOMEDRIVE$env:HOMEPATH`\GDX-GetProcessOwnerLog.txt"
+
+Function Get-ProcessOwner{
 <#
 .SYNOPSIS
 Queries target computer for current processes and collects process owners, process name, and command line arguments used for execution.
@@ -13,6 +15,15 @@ The current version of the function will only passthrough the current account th
 .PARAMETER ComputerName
 The name or IP address of the target host to query.
 
+.PARAMETER ShowProgress
+Shows current step in function process as well as overall progress bar to illustrate progress through target computer list
+
+.PARAMETER ErrorLogPath
+Allows user to define a different error logging location for the Get-ProcessOwner function.
+The default location is defined by environment variables HOMEDRIVE and HOMEPATH and the default file name is GDX-GetProcessOwnerLog.txt
+
+Log format is [targetName] -- date: errorMessage
+
 .EXAMPLE
 Get-ProcessOwner -ComputerName 192.168.100.15
 Returns all processes on target host at IP address 192.168.100.15.
@@ -20,12 +31,12 @@ Returns all processes on target host at IP address 192.168.100.15.
 .EXAMPLE
 Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object {$_.IsService -eq $True}
 (This example is compatible with PowerShell 2.0 and later)
-Returns all services running on target host ApplicationServer01.contoso.com.
+Returns only services running on target host ApplicationServer01.contoso.com.
 
 .EXAMPLE
 Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object -Property IsService -eq $True
 (This example is compatible with PowerShell 3.0 and later)
-Returns all services running on target host ApplicationServer01.contoso.com.
+Returns only services running on target host ApplicationServer01.contoso.com.
 
 .EXAMPLE
 Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object {$_.ProcessOwner -match "appSvcUser"}
@@ -36,6 +47,9 @@ Returns all processes running under the appSvcUser user context (irrespective of
 Get-ProcessOwner -ComputerName ApplicationServer01.contoso.com | Where-Object -Property ProcessOwner -eq "SYSTEM"
 (This example is compatible with PowerShell 3.0 and later)
 Returns all processes running under the local SYSTEM user context on target host ApplicationServer01.contoso.com.
+
+.LINK
+https://github.com/grayscaledx/poshGetProcessOwner
 #>
 [CmdletBinding()]
 param(
@@ -45,61 +59,92 @@ param(
     [string[]]$ComputerName = 'localhost',
     [Parameter(HelpMessage="A valid username to check processes against.")]
     $UserAccount = $null,
-    [switch]$ShowProgress
+    [switch]$ShowProgress,
+    [string]$ErrorLogPath = $logLocation
 )
 
 
 BEGIN {
-    Write-Verbose "Initializing results object..."
-    $processContainer = @()
+
     Write-Verbose "User Account is null: $($UserAccount -eq $null)"
+
+    $computerPercentage = (100 / ($ComputerName.count) -as [int])
+    $percentComplete = 0
+
+    Write-Verbose "Initializing local error variable for logging..."
+    $execError = @()
+
 }
 PROCESS {
         foreach ($CurrentComputer in $ComputerName){
 
-        try{
+            Write-Verbose "Initializing results object for $CurrentComputer..."
+            $processContainer = @()
 
-                Write-Verbose "Querying target $CurrentComputer for processes via WMI..."
-                $CurrentProcesses = Get-WmiObject -ComputerName $CurrentComputer win32_process -ErrorAction Stop
+            try{
 
-                Write-Verbose "Querying target $CurrentComputer for running services via WMI..."
-                $CurrentServices = Get-WmiObject -ComputerName $CurrentComputer win32_service -ErrorAction Stop
+                    if ($ShowProgress) { Write-Progress -Activity "Querying processes on $CurrentComputer ..." -PercentComplete $percentComplete }
+                    Write-Verbose "Querying target $CurrentComputer for processes via WMI..."
+                    
+                    $CurrentProcesses = Get-WmiObject -ComputerName $CurrentComputer win32_process -ErrorAction Stop -ErrorVariable execError
+
+                    if ($ShowProgress) { Write-Progress -Activity "Querying services on $CurrentComputer ..." -PercentComplete $percentComplete }
+                    Write-Verbose "Querying target $CurrentComputer for running services via WMI..."
+                    
+                    $CurrentServices = Get-WmiObject -ComputerName $CurrentComputer win32_service -ErrorAction Stop -ErrorVariable execError
             
-                Write-Verbose "Processes Received; Building Query Object..."
-                foreach ($ComputerProcess in $CurrentProcesses){           
+                    if ($ShowProgress) { Write-Progress -Activity "Appending process owner data from $CurrentComputer to PSObject ..." -PercentComplete $percentComplete }
+                    Write-Verbose "Processes Received; Building Query Object..."
+                    
+                    foreach ($ComputerProcess in $CurrentProcesses){           
 
-                    if ($CurrentServices.ProcessID -contains $ComputerProcess.ProcessID){
-                        Write-Verbose "ProcessID $($ComputerProcess.ProcessID) `/ $($ComputerProcess.ProcessName) is a service..."
-                        $isService = $True
-                    } else {
-                        $isService = $False
-                    }
+                        if ($CurrentServices.ProcessID -contains $ComputerProcess.ProcessID){
+                            Write-Verbose "ProcessID $($ComputerProcess.ProcessID) `/ $($ComputerProcess.ProcessName) is a service..."
+                            $isService = $True
+                        } else {
+                            $isService = $False
+                        }
                 
-                    $processProps = @{
-                                        'ProcessOwner'=$ComputerProcess.GetOwner().User;
-                                        'ProcessName'=$ComputerProcess.ProcessName;
-                                        'ProcessID'=$ComputerProcess.ProcessID;
-                                        'CommandLine'=$ComputerProcess.CommandLine;
-                                        'ComputerName'=$ComputerProcess.PSComputerName
-                                        'IsService'=$isService
-                                     }
-                    $processObj = New-Object -TypeName psobject -Property $processProps
-                    $processObj.PSObject.TypeNames.Insert(0,'GDX.ProcessOwner')
-                    $processContainer += $processObj
+                        $processProps = @{
+                                            'ProcessOwner'=$ComputerProcess.GetOwner().User;
+                                            'ProcessName'=$ComputerProcess.ProcessName;
+                                            'ProcessID'=$ComputerProcess.ProcessID;
+                                            'CommandLine'=$ComputerProcess.CommandLine;
+                                            'ComputerName'=$ComputerProcess.PSComputerName
+                                            'IsService'=$isService
+                                         }
+                        $processObj = New-Object -TypeName psobject -Property $processProps
+                        $processObj.PSObject.TypeNames.Insert(0,'GDX.ProcessOwner')
+                        $processContainer += $processObj
                             
+                    }
+
+                    $percentComplete += $computerPercentage
+                    Write-Output $processContainer
                 }
+
+            catch {
+
+                Write-Output "$(get-date) -- `[$($CurrentComputer)`]`: $($execError[-1])" | Out-File -Append $ErrorLogPath
+                $percentComplete += $computerPercentage
+
+                Write-Verbose "Finished processing query on target -- $CurrentComputer"
+
             }
 
-        catch [System.Exception] {Write-Output "WMI query failed on $CurrentComputer.  Please run with Verbose option for more information"}
-
-        catch {Write-Output "Something unexpected occurred..."}
-
-        Write-Verbose "Finished processing query on target -- $CurrentComputer"
+        
 
         }
         
 }
-END { Write-Output $processContainer }
+END { 
+ 
+    if ($ShowProgress) { Write-Progress -Activity "Function finished..." -Completed }
+    if ($execError.count -gt 0) { Write-Host "There were errors encountered during execution.  Please reference error log at $ErrorLogPath" }
+
+    Write-Verbose "Function complete..."
+
+    }
 
 }
 
